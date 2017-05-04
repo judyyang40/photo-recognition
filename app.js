191dbd8a05ca71ace186c649795865c2fae55b79
Wei Yao <wei.yao@sjsu.edu>
2017-05-04 15:25:55 -0700

    add gitignore file and aws-sdk

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var app = express();
 var port = process.env.PORT || 8060;
 var options = {root: __dirname + '/views/'};
 
+
+//using AWS SDK and settings
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./config.json');
+
 //routes for API
 var router = express.Router();
 router.get('/', function(req, res) {
@@ -18,3 +23,34 @@ app.use('/', router);
 //start server
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+
+
+//Amazon AWS sample code below
+//writing items to DynamoDB
+var table = new AWS.DynamoDB({apiVersion: '2012-08-10', params: {TableName: 'MY_TABLE'}});
+var key = 'UNIQUE_KEY_ID';
+// Write the item to the table
+var itemParams = {
+    Item: {
+        'id': {S: key},
+        'data': {S: 'data'}
+    },
+    ReturnConsumedCapacity: 'TOTAL'
+};
+
+table.putItem(itemParams, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+});
+
+//reading items from DynamoDB
+var table = new AWS.DynamoDB({apiVersion: '2012-08-10', params: {TableName: 'MY_TABLE'}});
+var key = 'UNIQUE_KEY_ID';
+// Note that the params are different for getItem().
+var itemParams = {Key: {'id': {S: key} }};
+
+table.getItem(itemParams, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+});
---
+++ routes/uploadPhoto.js
@@ -0,0 +1,70 @@
+//Code for uploading photo to S3
+
+//configure the SDK
+var albumBucketName = 'BUCKET_NAME';
+var bucketRegion = 'REGION';
+var IdentityPoolId = 'IDENTITY_POOL_ID';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+});
+
+
+
+//create an album in S3
+function createAlbum(albumName) {
+  albumName = albumName.trim();
+  if (!albumName) {
+    return alert('Album names must contain at least one non-space character.');
+  }
+  if (albumName.indexOf('/') !== -1) {
+    return alert('Album names cannot contain slashes.');
+  }
+  var albumKey = encodeURIComponent(albumName) + '/';
+  s3.headObject({Key: albumKey}, function(err, data) {
+    if (!err) {
+      return alert('Album already exists.');
+    }
+    if (err.code !== 'NotFound') {
+      return alert('There was an error creating your album: ' + err.message);
+    }
+    s3.putObject({Key: albumKey}, function(err, data) {
+      if (err) {
+        return alert('There was an error creating your album: ' + err.message);
+      }
+      alert('Successfully created album.');
+    });
+  });
+}
+
+
+//adding photo to album
+function addPhoto(albumName) {
+  var files = document.getElementById('photoupload').files;
+  if (!files.length) {
+    return alert('Please choose a file to upload first.');
+  }
+  var file = files[0];
+  var fileName = file.name;
+  var albumPhotosKey = encodeURIComponent(albumName) + '//';
+
+  var photoKey = albumPhotosKey + fileName;
+  s3.upload({
+    Key: photoKey,
+    Body: file,
+    ACL: 'public-read'
+  }, function(err, data) {
+    if (err) {
+      return alert('There was an error uploading your photo: ', err.message);
+    }
+    alert('Successfully uploaded photo.');    
+  });
+}
